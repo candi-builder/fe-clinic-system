dd60b095c96326023b93e7878ab79a905c8c1244
azkifairuz <azkiajmal@gmail.com>
2024-05-31 00:10:55 +0700

    add poli

--- components/table/PoliManagement.vue
+++ components/table/PoliManagement.vue
@@ -2,9 +2,14 @@
 import { PoliResponse } from "@/types/poli";
 import axios from "axios";
 const listPoli = ref<PoliResponse[]>([]);
-
+const dialog = ref<boolean>(false);
+const snackbarVisible = ref(false);
+const snackbarText = ref<string>("");
 const config = useRuntimeConfig();
 const baseUrl = config.public.baseUrl;
+const poliRequest = reactive({
+  name:""
+})
 async function getListPoli() {
   try {
     axios
@@ -14,21 +19,44 @@ async function getListPoli() {
         console.log(response.data);
       })
       .catch(function (error) {
+
         console.log(error);
       });
   } catch (error) {}
 }
 
+async function addPoli() {
+  try {
+    axios
+      .post(`${baseUrl}/poli`,poliRequest)
+      .then(function (response) {
+        console.log(response.data);
+        getListPoli()
+        snackbarText.value = "berhasil menambah poli"
+        snackbarVisible.value = true
+      })
+      .catch(function (error) {
+        console.log(error);
+        snackbarText.value = error
+        snackbarVisible.value = true
+      });
+  } catch (error) {}
+} 
 onMounted(()=>{
   getListPoli()
 })
 </script>
 <template>
+  <MySnackbar
+      v-model:modelValue="snackbarVisible"
+      :text="snackbarText"
+      :timeout="2000"
+    ></MySnackbar>
     <v-card elevation="10" class="">
     <v-card-item class="pa-6">
       <div class="d-flex justify-space-between align-center">
-        <v-card-title class="text-h5 pt-sm-2 pb-7">List pegawai</v-card-title>
-        <v-btn color="success" @click="openFormAddUser">Tambah poli</v-btn>
+        <v-card-title class="text-h5 pt-sm-2 pb-7">List Poli</v-card-title>
+        <v-btn color="success" @click="dialog = true">Tambah poli</v-btn>
       </div>
       <v-table class="month-table">
         <thead>
@@ -65,6 +93,41 @@ onMounted(()=>{
           </tr>
         </tbody>
       </v-table>
+      <v-dialog v-model="dialog" width="500">
+        <v-card prepend-icon="mdi-dock-top" title="data pegawai">
+          <v-card-text>
+            <v-row dense>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="poliRequest.name"
+                  color="deep-purple"
+                  type="text"
+                  label="nama poli"
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn text="Close" variant="plain" @click="dialog = false"
+              >tutup</v-btn
+            >
+
+            <v-btn
+              color="primary"
+              text="Save"
+              variant="tonal"
+              @click="addPoli()"
+              >kirim</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-card-item>
   </v-card>
 </template>
